654b08e4551bc653d06829c4939ec94f77d69c30
unknown <mani.sarathy121@gmail.com>
2026-09-22 18:03:50 +0530

    Fix sign-in: auth redirect always returns an absolute URL

--- src/app/api/me/route.ts
+++
@@ -1,7 +0,0 @@
-import { route, json } from "@/lib/api/handler";
-import { requireSession } from "@/lib/auth/guards";
-
-export const GET = route(async () => {
-  const session = await requireSession();
-  return json({ user: session.user });
-});
--- src/lib/version.ts
+++
@@ -1,3 +0,0 @@
-export const APP_VERSION = "v1.00070";
-export const APP_VERSION_RAW = "1.00070";
-export const COMMIT_COUNT = 70;
